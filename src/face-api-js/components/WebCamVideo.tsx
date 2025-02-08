2805c2dd62d02186f81488343584c4ee29751ced
Deepen Timalsina <deepentimalsina05@gmail.com>
2025-02-08 10:13:51 +0545

    update: commented face-api.js code

--- src/face-api-js/components/WebCamVideo.tsx
+++ src/face-api-js/components/WebCamVideo.tsx
@@ -1,33 +1,32 @@
-'use client';
 
-import { useLoadModels } from '../hooks/useLoadModels';
-import { usePlayVideo } from '../hooks/usePlayVideo';
+// import { useLoadModels } from '../hooks/useLoadModels';
+// import { usePlayVideo } from '../hooks/usePlayVideo';
 
-function WebCamVideo() {
-  const modelsLoaded = useLoadModels();
+// function WebCamVideo() {
+//   const modelsLoaded = useLoadModels();
 
-  const [videoRef, canvasRef, videoHeight, videoWidth] = usePlayVideo({
-    modelsLoaded,
-  });
+//   const [videoRef, canvasRef, videoHeight, videoWidth] = usePlayVideo({
+//     modelsLoaded,
+//   });
 
-  return (
-<div style={{ minHeight: '100vh', maxHeight: '100vh', display: 'grid', gridTemplateColumns: '1fr', gap: '1rem' }}>
-  <video
-    ref={videoRef}
-    width={videoWidth}
-    height={videoHeight || undefined}
-    playsInline
-    autoPlay
-  />
-  <canvas
-    ref={canvasRef}
-    width={videoWidth}
-    height={videoHeight || undefined}
-    style={{ border: '1px solid red' }}
-  />
-</div>
+//   return (
+// <div style={{ minHeight: '100vh', maxHeight: '100vh', display: 'grid', gridTemplateColumns: '1fr', gap: '1rem' }}>
+//   <video
+//     ref={videoRef}
+//     width={videoWidth}
+//     height={videoHeight || undefined}
+//     playsInline
+//     autoPlay
+//   />
+//   <canvas
+//     ref={canvasRef}
+//     width={videoWidth}
+//     height={videoHeight || undefined}
+//     style={{ border: '1px solid red' }}
+//   />
+// </div>
 
-  );
-}
+//   );
+// }
 
-export default WebCamVideo;
+// export default WebCamVideo;
--- src/face-api-js/hooks/usePlayVideo.ts
+++ src/face-api-js/hooks/usePlayVideo.ts
@@ -1,105 +1,105 @@
-import * as faceApi from 'face-api.js';
-import { useEffect, useRef, useState } from 'react';
+// import * as faceApi from 'face-api.js';
+// import { useEffect, useRef, useState } from 'react';
 
-const videoWidth = 640;
-const videoHeight = 480;
+// const videoWidth = 640;
+// const videoHeight = 480;
 
-export function usePlayVideo({ modelsLoaded }: { modelsLoaded: boolean }) {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [videoPlaying, setVideoPlaying] = useState(false);
+// export function usePlayVideo({ modelsLoaded }: { modelsLoaded: boolean }) {
+//   const videoRef = useRef<HTMLVideoElement>(null);
+//   const canvasRef = useRef<HTMLCanvasElement>(null);
+//   const [videoPlaying, setVideoPlaying] = useState(false);
 
-  useEffect(() => {
-    const startVideo = async () => {
-      if (videoRef.current && modelsLoaded) {
-        try {
-          videoRef.current.srcObject =
-            await navigator.mediaDevices.getUserMedia({
-              audio: false,
-              video: { width: videoWidth, height: videoHeight },
-            });
-          setVideoPlaying(true);
-        } catch (err) {
-          console.error('Error accessing webcam:', err);
-        }
-      }
-    };
+//   useEffect(() => {
+//     const startVideo = async () => {
+//       if (videoRef.current && modelsLoaded) {
+//         try {
+//           videoRef.current.srcObject =
+//             await navigator.mediaDevices.getUserMedia({
+//               audio: false,
+//               video: { width: videoWidth, height: videoHeight },
+//             });
+//           setVideoPlaying(true);
+//         } catch (err) {
+//           console.error('Error accessing webcam:', err);
+//         }
+//       }
+//     };
 
-    startVideo();
-  }, [modelsLoaded]);
+//     startVideo();
+//   }, [modelsLoaded]);
 
-  useEffect(() => {
-    let intervalId: NodeJS.Timeout;
+//   useEffect(() => {
+//     let intervalId: NodeJS.Timeout;
 
-    const detectFaces = async function () {
-      if (
-        !videoRef.current ||
-        videoRef.current.srcObject === null ||
-        !canvasRef.current
-      )
-        return;
+//     const detectFaces = async function () {
+//       if (
+//         !videoRef.current ||
+//         videoRef.current.srcObject === null ||
+//         !canvasRef.current
+//       )
+//         return;
 
-      const displaySize = { width: videoWidth, height: videoHeight };
-      faceApi.matchDimensions(canvasRef.current, displaySize);
+//       const displaySize = { width: videoWidth, height: videoHeight };
+//       faceApi.matchDimensions(canvasRef.current, displaySize);
 
-      intervalId = setInterval(async () => {
-        let detections;
-        if (videoRef.current) {
-          try {
-            detections = await faceApi
-              .detectAllFaces(
-                videoRef.current,
-                new faceApi.TinyFaceDetectorOptions()
-              )
-              .withFaceLandmarks()
-              .withFaceExpressions()
-              .withAgeAndGender();
-          } catch (err) {
-            console.error('Error during face detection:', err);
-            return;
-          }
-        }
+//       intervalId = setInterval(async () => {
+//         let detections;
+//         if (videoRef.current) {
+//           try {
+//             detections = await faceApi
+//               .detectAllFaces(
+//                 videoRef.current,
+//                 new faceApi.TinyFaceDetectorOptions()
+//               )
+//               .withFaceLandmarks()
+//               .withFaceExpressions()
+//               .withAgeAndGender();
+//           } catch (err) {
+//             console.error('Error during face detection:', err);
+//             return;
+//           }
+//         }
 
-        const resizedDetections = faceApi.resizeResults(
-          detections,
-          displaySize
-        );
+//         const resizedDetections = faceApi.resizeResults(
+//           detections,
+//           displaySize
+//         );
 
-        if (canvasRef.current) {
-          // Clear the canvas
-          canvasRef.current
-            .getContext('2d')
-            .clearRect(0, 0, videoWidth, videoHeight);
+//         if (canvasRef.current) {
+//           // Clear the canvas
+//           canvasRef.current
+//             .getContext('2d')
+//             .clearRect(0, 0, videoWidth, videoHeight);
 
-          // Draw the detections
-          faceApi.draw.drawDetections(canvasRef.current, resizedDetections);
-          faceApi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
-          faceApi.draw.drawFaceExpressions(
-            canvasRef.current,
-            resizedDetections
-          );
+//           // Draw the detections
+//           faceApi.draw.drawDetections(canvasRef.current, resizedDetections);
+//           faceApi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+//           faceApi.draw.drawFaceExpressions(
+//             canvasRef.current,
+//             resizedDetections
+//           );
 
-          // Draw age and gender
-          resizedDetections.forEach((detection) => {
-            const box = detection.detection.box;
-            const drawBox = new faceApi.draw.DrawBox(box, {
-              label: `Age: ${Math.round(detection.age)} | Gender: ${detection.gender}`,
-            });
-            drawBox.draw(canvasRef.current);
-          });
-        }
-      }, 1000);
-    };
+//           // Draw age and gender
+//           resizedDetections.forEach((detection) => {
+//             const box = detection.detection.box;
+//             const drawBox = new faceApi.draw.DrawBox(box, {
+//               label: `Age: ${Math.round(detection.age)} | Gender: ${detection.gender}`,
+//             });
+//             drawBox.draw(canvasRef.current);
+//           });
+//         }
+//       }, 1000);
+//     };
 
-    detectFaces();
+//     detectFaces();
 
-    // Cleanup function
-    return () => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-    };
-  }, [videoPlaying]);
+//     // Cleanup function
+//     return () => {
+//       if (intervalId) {
+//         clearInterval(intervalId);
+//       }
+//     };
+//   }, [videoPlaying]);
 
-  return [videoRef, canvasRef, videoHeight, videoWidth] as const;
-}
+//   return [videoRef, canvasRef, videoHeight, videoWidth] as const;
+// }
